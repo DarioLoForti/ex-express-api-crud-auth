20fa7850ab4fbbe032bbd125dfde249435f1adec
Dario Lo Forti <lofortidario@gmail.com>
2024-06-10 15:45:03 +0200

    Add multer file upload functionality to registration route

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -6,7 +6,26 @@ const validator = require("../middleware/validator.js");
 
 const { registerData, loginData } = require("../validations/users.js");
 
-router.post("/register", validator(registerData), authController.register);
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage });
+
+router.post(
+  "/register",
+  [upload.single("uploads"), validator(registerData)],
+  authController.register
+);
 
 router.post("/login", validator(loginData), authController.login);
 
