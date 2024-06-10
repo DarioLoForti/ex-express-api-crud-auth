203deaefaff164c1c1ce27f083a6ce1b6c0d7dd8
Dario Lo Forti <lofortidario@gmail.com>
2024-06-10 16:18:56 +0200

    Update multer file upload functionality in authRouter

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -10,12 +10,10 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: "./public/uploads/",
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
+  destination: "public/uploads",
+  filename: (req, file, cf) => {
+    const fileType = path.extname(file.originalname);
+    cf(null, String(Date.now()) + fileType);
   },
 });
 
@@ -23,7 +21,7 @@ const upload = multer({ storage });
 
 router.post(
   "/register",
-  [upload.single("uploads"), validator(registerData)],
+  [upload.single("profileImage"), validator(registerData)],
   authController.register
 );
 
